624675d540c157338c84398f5adc69fceb6aa1f9
Jon <gravityrideseverything1@gmail.com>
2024-10-14 23:02:39 -0400

    prompt to run alternative command

--- pkg/llm.go
+++ pkg/llm.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/spf13/viper"
 	"github.com/tmc/langchaingo/llms"
@@ -12,7 +13,14 @@ import (
 
 func GetPrompt() string {
 	prompt := `Given the following system information and command input + output, 
-dignose the problem and provide a solution. Do not repeat the system info.`
+dignose the problem and provide a solution. Do not repeat the system info.
+
+If there area any obvious typos in the command, provide a suggested command 
+at the end of your reply in plain text, prefaced with "Recommended command: ". The
+command should be on the same line as "Recommended command: " and there should be no
+text following it. Do not include any backticks surrounding the command and do not include
+any lines of text after it.
+`
 
 	verbosity := viper.Get("VERBOSITY")
 
@@ -34,7 +42,7 @@ func CallLLM(message string) string {
 	}
 	ctx := context.Background()
 	completion, err := llm.Call(ctx, message,
-		llms.WithTemperature(0.8),
+		llms.WithTemperature(0.1),
 		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
 			fmt.Print(string(chunk))
 			return nil
@@ -42,10 +50,21 @@ func CallLLM(message string) string {
 	)
 
 	if err != nil {
-		log.Fatalf("An error occurred while calling the LLM: %s", err)
+		fmt.Println(checkError(err))
 	}
 
 	return completion
+}
+
+func checkError(err error) string {
+	e := fmt.Sprintf("An error occurred while calling the LLM: %s", err)
+	if strings.Contains(e, "connect: connection refused") {
+		return fmt.Sprintf("%s\nIt looks like Ollama may not be running.", e)
+	}
+	if strings.Contains(e, "try pulling it first") {
+		return fmt.Sprintf("%s\nThe required Ollama model is not installed.", e)
+	}
+	return e
 }
 
 func getModel() string {
--- pkg/run.go
+++ pkg/run.go
@@ -8,7 +8,10 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
-func Run(command string) string {
+func Run(command string) {
+	// Prompt user to re-run the last command.
+	// If "no" is selected, then we can't capture stdout/stderr
+	// and the completion will have to be generated only by the command.
 	label := fmt.Sprintf("Rerun %s", command)
 
 	prompt := promptui.Prompt{
@@ -24,25 +27,59 @@ func Run(command string) string {
 	out := dataString + "\nCommand: " + command
 
 	if strings.ToLower(shouldRerun) == "y" {
-		out += "\n" + rerunCommand(command)
+		out += "\n" + runCommand(command)
 	}
-
 	fmt.Println(out)
 
+	// Get advice from the LLM. The LLM /should/ provide a recommended
+	// alternative command, if one is available. We can parse this from
+	// the response.
 	completion := CallLLM(GetPrompt() + out)
+	recommendedCommand := getRecommendedCommand(completion)
 
-	return completion
+	if recommendedCommand == "" {
+		return
+	}
+
+	// Ask the user to run the suggested command, if one is available.
+	label = fmt.Sprintf("Run %s", recommendedCommand)
+
+	prompt = promptui.Prompt{
+		Label:     label,
+		IsConfirm: true,
+	}
+
+	shouldRun, _ := prompt.Run()
+	newOut := recommendedCommand
+
+	if strings.ToLower(shouldRun) == "y" {
+		newOut += "\n" + runCommand(recommendedCommand)
+		fmt.Println(newOut)
+	}
 }
 
-func rerunCommand(command string) string {
+func runCommand(command string) string {
 	shell := GetShell()
 
 	cmd := exec.Command(shell, "-c", command)
-	output, err := cmd.CombinedOutput()
+	output, err := cmd.Output()
 
 	if err != nil {
 		return fmt.Sprintf("Error: %s\nOutput: %s", err.Error(), string(output))
 	}
 
 	return string(output)
+}
+
+func getRecommendedCommand(response string) string {
+	lines := strings.Split(response, "\n")
+	lastLine := lines[len(lines)-1]
+
+	if !strings.HasPrefix(lastLine, "Recommended command: ") {
+		return ""
+	}
+
+	command := strings.Split(lastLine, "Recommended command: ")[1]
+
+	return command
 }
